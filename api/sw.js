2ce4b4c6f5417e9f2faf2a4b860dd48db6d37aec
zoeyaviation <zoeydev@chickennet.work>
2026-06-12 20:44:25 -0400

    Update sw.js

--- api/sw.js
+++ api/sw.js
@@ -4,9 +4,23 @@ self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); })
 self.addEventListener('fetch', function(event) {
     var url = new URL(event.request.url);
     
-    if (url.pathname.startsWith('/api/proxy')) return;
-    if (url.pathname === '/' || url.pathname === '/index.html' || url.pathname === '/sw.js') return;
+    // Intercept /proxy/* paths
+    if (url.pathname.startsWith('/proxy/')) {
+        var encodedUrl = url.pathname.replace('/proxy/', '');
+        var targetUrl = decodeURIComponent(encodedUrl) + url.search;
+        
+        // Fetch through our API
+        event.respondWith(fetch('/api/proxy?url=' + encodeURIComponent(targetUrl)));
+        return;
+    }
     
+    // Don't intercept our own API calls or main page
+    if (url.pathname.startsWith('/api/proxy') || url.pathname === '/' || 
+        url.pathname === '/index.html' || url.pathname === '/sw.js') {
+        return;
+    }
+    
+    // For everything else (YouTube API calls, etc), proxy them
     var targetPath = url.pathname + url.search;
     var host = 'www.youtube.com';
     
@@ -18,12 +32,8 @@ self.addEventListener('fetch', function(event) {
         host = 'www.gstatic.com';
     } else if (targetPath.indexOf('ytimg.com') > -1) {
         host = 'i.ytimg.com';
-    } else if (targetPath.indexOf('reddit.com') > -1 || targetPath.indexOf('redd.it') > -1) {
-        host = 'www.reddit.com';
-    } else if (targetPath.indexOf('x.com') > -1 || targetPath.indexOf('twitter.com') > -1 || targetPath.indexOf('xcancel.com') > -1 || targetPath.indexOf('nitter') > -1) {
-        host = 'xcancel.com';
-    } else if (targetPath.indexOf('invidious') > -1) {
-        host = 'invidious.fdn.fr';
+    } else if (targetPath.indexOf('ggpht.com') > -1) {
+        host = 'yt3.ggpht.com';
     }
     
     event.respondWith(fetch('/api/proxy?url=' + encodeURIComponent('https://' + host + targetPath)));
